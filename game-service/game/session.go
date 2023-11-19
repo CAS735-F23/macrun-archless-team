c7d6a9846e70963e179211b751243a6a5c418fb8
xjasonlyu <xjasonlyu@gmail.com>
2023-11-19 15:57:11 -0500

    refactor: use game session methods

--- game-service/game/session.go
+++ game-service/game/session.go
@@ -16,13 +16,10 @@ func (app *App) handleGameStartEvent(msg *dto.Message) error {
 
 	// Game stop logic
 	if msg.Action == consts.GameStopAction {
-		if s, ok := app.sessions[msg.PlayerDTO.Username]; ok {
-			s.Close()
-			delete(app.sessions, msg.PlayerDTO.Username)
-			log.Printf("User %s session deleted!", msg.PlayerDTO.Username)
-		} else {
-			return fmt.Errorf("session for %s not found", msg.PlayerDTO.Username)
+		if err := app.DeleteSessionByUser(msg.PlayerDTO.Username); err != nil {
+			return err
 		}
+		log.Printf("User %s session has been deleted!", msg.PlayerDTO.Username)
 	}
 
 	// Check session existence
@@ -33,18 +30,58 @@ func (app *App) handleGameStartEvent(msg *dto.Message) error {
 	fmt.Println(app.reg.Lookup("player-service"))
 
 	session := &Session{
-		Challenge: 0,
+		Challenge: struct{}{},
+		Player:    msg.PlayerDTO,
+		Score:     0, // init
 	}
 
-	app.sessions[msg.PlayerDTO.Username] = session
-
+	if err := app.StoreSessionByUser(msg.PlayerDTO.Username, session); err != nil {
+		return err
+	}
 	log.Printf("User %s session created!", msg.PlayerDTO.Username)
 
 	return nil
 }
 
+func (app *App) StoreSessionByUser(name string, session *Session) error {
+	_, ok := app.sessions[name]
+	if ok {
+		return fmt.Errorf("session already exists: %s", name)
+	}
+	app.sessions[name] = session
+	return nil
+}
+
+func (app *App) GetSessionByUser(name string) (*Session, error) {
+	s, ok := app.sessions[name]
+	if !ok {
+		return nil, fmt.Errorf("session not found: %s", name)
+	}
+	return s, nil
+}
+
+func (app *App) DeleteSessionByUser(name string) error {
+	if ses, err := app.GetSessionByUser(name); err != nil {
+		return err
+	} else {
+		ses.Close() // close session before delete.
+	}
+	delete(app.sessions, name)
+	return nil
+}
+
+type ReactOption uint8
+
+const (
+	Sheltering ReactOption = iota
+	Escaping
+	Fighting
+)
+
 type Session struct {
-	Challenge int
+	Challenge struct{}
+	Player    dto.PlayerDTO
+	Score     int
 }
 
 func (s *Session) HandleReaction() {
