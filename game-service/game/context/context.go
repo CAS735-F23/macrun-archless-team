76324ce1227be19d0f0326cb601cba493a3cedbf
xjasonlyu <xjasonlyu@gmail.com>
2023-12-02 17:27:20 -0500

    refactor: rename to context

--- game-service/game/context/context.go
+++ game-service/game/context/context.go
@@ -10,11 +10,25 @@ type Context struct {
 	//Challenge struct{}
 	Player    *dto.PlayerDTO
 	heartRate atomic.Int64
+	shelters  []dto.PointDTO
+	location  dto.PointDTO
 	Score     int
 }
 
 func (c *Context) UpdateHeartRate(v int) {
 	c.heartRate.Store(int64(v))
+}
+
+func (c *Context) UpdateShelters(v []dto.PointDTO) {
+	c.shelters = v
+}
+
+func (c *Context) GetLocation() dto.PointDTO {
+	return c.location
+}
+
+func (c *Context) UpdateLocation(v dto.PointDTO) {
+	c.location = v
 }
 
 func (c *Context) GetHeartRate() int {
